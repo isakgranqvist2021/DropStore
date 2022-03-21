8b27ad38ab475c333712d9ffc35aa0fba6dc7d59
isakgranqvist2021 <isakwebdev@gmail.com>
2022-03-21 22:08:35 +0100

    Improve design of product component

--- client/src/components/product/product-body/product-body.ts
+++ client/src/components/product/product-body/product-body.ts
@@ -1,5 +1,3 @@
-import { Form } from '../../form';
-import { List } from '../../list';
 import { Text } from '../../text';
 
 import { ProductBodyProps } from './product-body.types';
@@ -14,17 +12,11 @@ export class ProductBody {
 	render(root: HTMLElement) {
 		const productBody = document.createElement('div');
 
-		const listItems = ['Quality', 'Recommended By Pros'];
-
 		productBody.className = 'product_body';
 
 		new Text('h2').render(productBody, this.props.title);
 		new Text('p').render(productBody, this.props.description);
 		new Text('h4').render(productBody, this.props.price.toString());
-
-		new List({ listItems }).render(productBody);
-
-		new Form().render(productBody);
 
 		root.appendChild(productBody);
 	}
--- client/src/components/product/product.ts
+++ client/src/components/product/product.ts
@@ -1,19 +1,23 @@
+import { Form } from '../form';
+import { List } from '../list';
+
 import { ProductBody } from './product-body';
 import { Thumbnail } from './thumbnail';
-
-const productBodyProps = {
-	title: 'Amazing Shoes',
-	description:
-		'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-	price: '200 SEK',
-};
+import { PRODUCT_BODY_PROPS, LIST_ITEMS } from './product.constants';
 
 export class Product {
 	render(root: HTMLElement) {
 		const product = document.createElement('div');
 
+		product.className = 'product';
+
 		new Thumbnail().render(product);
-		new ProductBody(productBodyProps).render(product);
+
+		new ProductBody(PRODUCT_BODY_PROPS).render(product);
+
+		new List({ listItems: LIST_ITEMS }).render(product);
+
+		new Form().render(product);
 
 		root.appendChild(product);
 	}
